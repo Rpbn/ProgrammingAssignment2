bc360a8172236b782f187936e88579d74c52f7d2
Rubens Paes Barreto <rubens.barreto@gmail.com>
2015-01-20 18:11:23 -0200

    Submit the Exercise Version 1
    This will be the first submission from the assignment 2 for the R
    Software course in courser.

--- cachematrix.R
+++ cachematrix.R
@@ -11,5 +11,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getmean()
+    if(!is.null(m)) {                   ## Check if there was already value in the M populated by the solve function
+        message("getting cached data")  ## If there is this value will be used.
+        return(m)
+    } else {                            ## If M is different from NULL it will not execute the solve again.
+        data <- x$get()                     
+        m <- solve(data, ...)
+        x$setmean(m)
+    }
+    m
 }
