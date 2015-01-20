34a20580b5d5066a551d5f77dd51fe0b00a0b59f
Rubens Paes Barreto <rubens.barreto@gmail.com>
2015-01-20 18:23:04 -0200

    Trying again
    Seams that the commit did not work. The file was only half update.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions are used to avoid computational processes to invert a matrix if  
+## this was already done (executed) before.
 
-## Write a short comment describing this function
+## This function creates a list of functions that are supposed to be used when someone tries
+## calculate the inverse of a matrix. The functions are:
+# set the value of the vector
+# get the value of the vector
+# set the value of the mean
+# get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL               ## Used to set m, this NULL will be found when there is no cache yet.
+    set <- function(y) {
+        x <<- y             ## This function can be used to change the specialMatrix values. 
+        m <<- NULL          ## Set the value to the Parent environment as this camos from a change in the matrix.
+    }
+    get <- function() x
+    setmean <- function(inverse) m <<- inverse
+    getmean <- function() m
     list( get = get,
+          setmean = setmean,
+          getmean = getmean)
 }
 
-
-## Write a short comment describing this function
+## The cacheSolve function is used to check if the inverse matrix was already calculated
+## If it has been calculated, this will return the cached value and avoid all the computation again
+## if it has NOT been calculated it will execute the computation and cache the result using the "setmean" function 
 
 cacheSolve <- function(x, ...) {
     m <- x$getmean()
@@ -23,3 +38,4 @@ cacheSolve <- function(x, ...) {
     }
     m
 }
+
